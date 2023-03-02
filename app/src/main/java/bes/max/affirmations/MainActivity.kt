7a06ce92fb9ebf8cd68d39009881da7113b206f0
Max <bespalov.m.9@yandex.ru>
2023-03-02 08:31:26 +0300

    recyclerview was added

--- app/src/main/java/bes/max/affirmations/MainActivity.kt
+++ app/src/main/java/bes/max/affirmations/MainActivity.kt
@@ -2,13 +2,20 @@ package bes.max.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import bes.max.affirmations.adapter.ItemAdapter
 import bes.max.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+
 
     }
 }
